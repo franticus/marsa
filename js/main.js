3417b1d0a8546f326d9878d9ab45140e65e48bc3
S.Zelensky <zelenstein@gmail.com>
2024-03-29 17:01:48 +0300

    anim title

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
-const faqItems = document.querySelectorAll(".faq__title");
+const faqItems = document.querySelectorAll('.faq__title');
 
-const accordionToggle = (e) => {
-  e.currentTarget.classList.toggle("faq__active");
+const accordionToggle = e => {
+  e.currentTarget.classList.toggle('faq__active');
   const item = e.currentTarget.nextElementSibling;
-  if (e.currentTarget.classList.contains("faq__active")) {
-    item.style.maxHeight = item.scrollHeight + "px";
+  if (e.currentTarget.classList.contains('faq__active')) {
+    item.style.maxHeight = item.scrollHeight + 'px';
   } else {
     item.style.maxHeight = 0;
   }
 };
 
 if (faqItems) {
-  faqItems.forEach((el) => {
-    el.addEventListener("click", accordionToggle);
+  faqItems.forEach(el => {
+    el.addEventListener('click', accordionToggle);
   });
 }
 
-const btn = document.querySelector(".header__btn");
-const menu = document.querySelector(".header-nav");
-const header = document.querySelector(".header");
-const body = document.querySelector("body");
+const btn = document.querySelector('.header__btn');
+const menu = document.querySelector('.header-nav');
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
 function showMenu() {
-  btn.classList.toggle("active");
-  menu.classList.toggle("active__menu");
-  header.classList.toggle("active__header");
-  body.classList.toggle('active-body')
+  btn.classList.toggle('active');
+  menu.classList.toggle('active__menu');
+  header.classList.toggle('active__header');
+  body.classList.toggle('active-body');
 }
 
 if (btn) {
-  btn.addEventListener("click", showMenu);
+  btn.addEventListener('click', showMenu);
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed('#typed-output', {
+    strings: [
+      'Transform your business with clients in <strong>just 5 days!</strong>',
+    ],
+    typeSpeed: 20,
+    startDelay: 500,
+    showCursor: false,
+    contentType: 'html',
+  });
+});
